99a3edb9c5fc45071bc67845bd451c3d77cf27d9
daoge_cmd <3523206925@qq.com>
2024-07-25 04:41:08 +0800

    feat: implement fly command

--- src/endstone_essentials/essentials_plugin.py
+++ src/endstone_essentials/essentials_plugin.py
@@ -1,3 +1,4 @@
+from endstone import Player
 from endstone.command import Command, CommandSender
 from endstone.event import EventPriority, ServerLoadEvent, event_handler
 from endstone.plugin import Plugin
@@ -7,9 +8,27 @@ class EssentialsPlugin(Plugin):
     name = "EssentialsPlugin"
     api_version = "0.4"
 
-    commands = {}
+    commands = {
+        "fly": {
+            "description": "Switch flying mode",
+            "usages": ["/fly"],
+            "permissions": ["essentials.command.fly"]
+        }
+    }
 
-    permissions = {}
+    permissions = {
+        "essentials.command": {
+            "description": "Allow users to use all commands provided by this plugin.",
+            "default": True,
+            "children": {
+                "essentials.command.fly": True
+            }
+        },
+        "essentials.command.fly": {
+            "description": "Allow users to use the /fly command.",
+            "default": True,
+        },
+    }
 
     def on_load(self) -> None:
         self.logger.info("Essentials plugin is loaded!")
@@ -21,4 +40,21 @@ class EssentialsPlugin(Plugin):
         self.logger.info("Essentials plugin is disabled!")
 
     def on_command(self, sender: CommandSender, command: Command, args: list[str]) -> bool:
+        if not isinstance(sender, Player):
+            sender.send_error_message("This command can only be executed by a player")
+            return False
+
+        match command.name:
+            case "fly":
+                if len(args) != 0:
+                    sender.send_error_message("Usage: /fly")
+                    return False
+
+                if sender.allow_flight:
+                    sender.allow_flight = False
+                    sender.send_message("Turn off flying mode")
+                else:
+                    sender.allow_flight = True
+                    sender.send_message("You can now fly")
+
         return True
